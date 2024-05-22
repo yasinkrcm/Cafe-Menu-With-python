88696d08fbae10939442e8a223ae42f1827dfd79
Yasin karaçam <yasinkaracam67@gmail.com>
2024-05-22 18:55:48 +0300

    Program was Created

--- Functions.py
+++ Functions.py
@@ -5,9 +5,7 @@ cursor = conn.cursor()
 
 # coffee class identification (according to coffee ingredients)
 class Coffee(): 
-    
     __doc__  = "This Class created for coffe type"
-
     def __init__(self,coffeeName,price,coffeeRate,water=0,milk = 0,milkFoam = 0,chocolate = 0,ice = 0 ):
         self.coffeeName = coffeeName
         self.price = price
@@ -18,8 +16,8 @@ class Coffee():
         self.chocolate = chocolate
         self.ice = ice
         
-# insert coffes in database
-    def insertCoffees(self): 
+# create table and insert variable 
+    def insertDatabase(self): 
         cursor.execute("""CREATE TABLE IF NOT EXISTS Coffees(
                     ID INTEGER PRIMARY KEY AUTOINCREMENT,
                     CoffeeName TEXT,
@@ -35,13 +33,45 @@ class Coffee():
                 (self.coffeeName,self.price,self.coffeeRate,self.water,self.milk,self.milk_foam,self.chocolate,self.ice))
         conn.commit()
 
-    # percentage representation of ingredients in coffees
-    def coffeeIngredients(self):
-        cursor.execute("""SELECT * FROM Coffees""")
-        coffees = cursor.fetchall()
-        print("Coffee Menu And Ingredients")
-        for coffee in coffees:
-            print(f"""
+
+
+    # Return Coffee size and price for bill 
+    def cupSizeForPrice(self):
+        while True:
+            size = input(f"""
+            Size For Cup                 
+            {"-"*28}                            
+            1- Tall (355ml)   : {self.price}
+                                        
+            2- Grande (473ml) : {self.price*1.15}
+                                              
+            3- Venti (591ml ) : {self.price*1.27}   
+            
+            Please choice for size : """)
+            if size == "1":
+                return self.price
+            if size == "2":
+                self.price*=1.15
+                return self.price
+            if size == "3":
+                self.price*=1.27
+                return self.price
+            else:
+                print("Invalid size Please choice from table ")
+
+# insert coffee in database       
+def insertCoffees():
+    coffees  = [espresso,americano,iceAmericano,latte,iceLatte,cappucino,mocha]
+    for coffee in coffees:
+            coffee.insertDatabase()
+
+# percentage representation of ingredients in coffees
+def coffeeIngredients():
+    cursor.execute("""SELECT * FROM Coffees""")
+    coffees = cursor.fetchall()
+    print("\nCoffee Menu And Ingredients")
+    for coffee in coffees:
+        print(f"""
     Name : {coffee[1]}
     Coofee Price : {coffee[2]}
     Coffee Rate : %{coffee[3]*100}
@@ -49,51 +79,30 @@ class Coffee():
     Milk   Rate : %{coffee[5]*100}
     Milk Foam  Rate : %{coffee[6]*100}
     Chocolate  Rate : %{coffee[7]*100}
-    ice  : {coffee[3] == True}
+    ice  : {coffee[8] == True}
     """)
-            print("-" * 40)
+        print("-" * 40)
 
-    # Return Coffee size and price for bill 
-    def cupSizeForPrice(self):
-        while True:
-            size = input(f"""
-            _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
-            |                                     |
-            |Size For Cup                         |
-            |                                     |
-            |Tall (355ml)   : {self.price}        |
-            |                                     |
-            |Grande (473ml) : {self.price*1.15}   |
-            |                                     |
-            |Venti (591ml ) : {self.price*1.27}   |
-            |                                     |
-            |_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _|
-    """)
-            size = size.lower().strip().capitalize()
-            if size == "Tall":
-                return self.price
-            if size == "Grande":
-                self.price*1.15
-                return self.price
-            if size == "Venti":
-                self.price*1.27
-                return self.price
-            else:
-                print("Invalid size Please choice from table ")
-            
-
-    def billCoffee(self):
-        return self.price
+# function to print bill on display
+def bill(result):
+    VAT = 18
+    print("-"*50)
+    print(f"""
+            Your Bill:      
+            {result}     
+            without VAT(Value-Added Tax ) : 
+            {result/(1+VAT/100)}    
+            """)
+    print("-"*50)
 
 # coffee identification
 espresso = Coffee("Espresso",30,0.8)                            
 americano = Coffee("Americano",35,0.3,0.7)                      
 iceAmericano = Coffee("Ice Americano",40,0.3,0.7,ice=1)       
 latte = Coffee("Latte",45,0.2,0,0.6,0.2)                        
-iceLatte = Coffee("Ice Latte",50,0.20,0.6,0.2,0,ice=1)             
+iceLatte = Coffee("Ice Latte",50,0.2,0,0.6,0.2,ice=1)             
 cappucino = Coffee("Cappucino",42,0.2,0,0.2,0.6)                
-mocha = Coffee("Mocha",55,0.3,0,0.4,0.1,0.2)                    
+mocha = Coffee("Mocha",55,0.3,0,0.4,0.1,0.2) 
 
-# create to list for loop
-coffees  = [espresso,americano,iceAmericano,latte,iceLatte,cappucino,mocha]
-
+#result for bill
+result = 0 
--- cafe-menu.py
+++ cafe-menu.py
@@ -1,133 +1,59 @@
-# Python'da class yapısı kullanarak ,
+from Functions import *
 
-# DB VE CLASS KULLAN
-
-# Kafe'de self-servis bir menü oluştur; Kahve satan bir Kafe oluştur , 
-
-# Fiş ile hesabı oluştur.
-
-# İçeceklerin boyutu olacak
-
-# İçeceklerin Tür , Sıcak veya soğukluğu 
-
-# Süt'ün türleri 
-
-# Krema vs 
-
-# *   5 çeşit kahvesi var
-#     (class ve kalıtım)
-
-# Ammericano 
-# latte
-# espresso
-# cappucino
-# mocha
-
-# *   kişi menüden bir kahve alarak kasa hesap yapıp fiş kesecek  
-#     (alınan kahve - fiyatı - fiş oluştur - fişe brüt ve net fiyatı [KDVLİ - KDVSİZ])
-
-import sqlite3 as sql
-# sql connection and cursor identf
-conn = sql.connect("Cafe.db")
-cursor = conn.cursor()
-
-# coffee class identification (according to coffee ingredients)
-class Coffee(): 
-    
-    __doc__  = "This Class created for coffe type"
-
-    def __init__(self,coffeeName,price,coffeeRate,water=0,milk = 0,milkFoam = 0,chocolate = 0,ice = 0 ):
-        self.coffeeName = coffeeName
-        self.price = price
-        self.coffeeRate = coffeeRate
-        self.water = water
-        self.milk = milk
-        self.milk_foam = milkFoam
-        self.chocolate = chocolate
-        self.ice = ice
-
-
-# coffee identification
-espresso = Coffee("Espresso",30,0.8)                            
-americano = Coffee("Americano",35,0.3,0.7)                      
-iceAmericano = Coffee("Ice Americano",40,0.3,0.7,ice=1)       
-latte = Coffee("Latte",45,0.2,0,0.6,0.2)                        
-iceLatte = Coffee("Ice Latte",50,0.20,0.6,0.2,0,ice=1)             
-cappucino = Coffee("Cappucino",42,0.2,0,0.2,0.6)                
-mocha = Coffee("Mocha",55,0.3,0,0.4,0.1,0.2)                    
-
-# create to list for loop
-coffees  = [espresso,americano,iceAmericano,latte,iceLatte,cappucino,mocha]
-
-# insert coffes in database
-def insertCoffees(): 
-    cursor.execute("""CREATE TABLE IF NOT EXISTS Coffees(
-                ID INTEGER PRIMARY KEY AUTOINCREMENT,
-                CoffeeName TEXT,
-                Price INTEGER,                
-                CoffeeRate INTEGER,
-                WaterRate INTEGER,
-                MilkRate INTEGER,
-                MilkFoamRate INTEGER,
-                ChocolateRate INTEGER,
-                Ice BOOLEAN
-    )""")
-    for coffee in coffees:
-        cursor.execute("""INSERT INTO Coffees(CoffeeName,Price,CoffeeRate,WaterRate,MilkRate,MilkFoamRate,ChocolateRate,Ice) VALUES (?,?,?,?,?,?,?,?)""",
-                    (coffee.coffeeName,coffee.price,coffee.coffeeRate,coffee.water,coffee.milk,coffee.milk_foam,coffee.chocolate,coffee.ice))
-    conn.commit()
-
-# percentage representation of ingredients in coffees
-def coffeeIngredients():
-    cursor.execute("""SELECT * FROM Coffees""")
-    coffees = cursor.fetchall()
-    print("Coffee Menu And Ingredients")
-
-    for coffee in coffees:
-        print(f"""
-Name : {coffee[1]}
-Coofee Price : {coffee[2]}
-Coffee Rate : %{coffee[3]*100}
-Water  Rate : %{coffee[4]*100}
-Milk   Rate : %{coffee[5]*100}
-Milk Foam  Rate : %{coffee[6]*100}
-Chocolate  Rate : %{coffee[7]*100}
-ice  : {coffee[3] == True}
-""")
-        print("-" * 40)
-coffeeIngredients()
-
-# while True:
-#     choice = input("""
-#       _ _ _ _ _ _ _ _ _ _
-#     |                    |                   
-#     |    Welcome To My   |
-#     |        Cafe        |
-#     |                    |                   
-#     | - 0 Insert Coffees |
-#     |     Database       |
-#     |                    |
-#     | - 1 Ingredients    |
-#     |       Coffees      |
-#     |                    |
-#     | - 2 Americano      |
-#     |                    |
-#     | - 3 Latte          |
-#     |                    |
-#     | - 4 Espresso       |
-#     |                    |
-#     | - 5 Cappuccino     |
-#     |                    |
-#     | - 6 Mocha          |
-#     |                    |
-#     |    -1 for quit     |
-#     | _ _ _ _ _ _ _ _ _ _|
+#Cafe Main Menu
+while True:
+    choice = input("""
+      _ _ _ _ _ _ _ _ _ _
+    |                    |                   
+    |    Welcome To My   |
+    |        Cafe        |
+    |                    |                   
+    | - 0 Insert Coffees |
+    |     Database       |
+    |                    |
+    | - 1 Ingredients    |
+    |       Coffees      |
+    |                    |
+    | - 2 Americano      |
+    |                    |
+    | - 3 Ice            |
+    |     Americano      |
+    |                    |
+    | - 4 Latte          |
+    |                    |
+    | - 5 Ice            |
+    |     Latte          |
+    |                    |
+    | - 6 Espresso       |
+    |                    |
+    | - 7 Cappuccino     |
+    |                    |
+    | - 8 Mocha          |
+    |                    |
+    |    -1 for quit     |
+    |       and your     |
+    |         bill       |
+    | _ _ _ _ _ _ _ _ _ _|
                    
-# Please select an choise   : """)
-#     if choice == "0":
-#         insertCoffees()
-
-#     if choice == "-1" :
-#         print(choice)
-#         break
-#     # if choice 
+Please select an choise   : """)
+    if choice == "0":
+        insertCoffees() 
+    if choice == "1":
+        coffeeIngredients()
+    if choice == "2":
+        result += americano.cupSizeForPrice()
+    if choice == "3":
+        result += iceAmericano.cupSizeForPrice()
+    if choice == "4":
+        result+=latte.cupSizeForPrice()
+    if choice == "5":
+        result+=iceLatte.cupSizeForPrice()
+    if choice == "6":
+        result+=espresso.cupSizeForPrice()
+    if choice == "7":
+        result+=cappucino.cupSizeForPrice()
+    if choice == "8":
+        result+=mocha.cupSizeForPrice()    
+    if choice == "-1" :
+        bill(result)
+        break
